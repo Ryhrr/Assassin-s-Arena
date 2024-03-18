347a78a94a6187c47e44f94efdaa600daddc66a9
Lucas0622 <lucas.kaepernik@student.htldornbirn.at>
2024-03-18 20:16:44 +0100

    Option menu
    added things to options

--- Assassin´s Arena/Assets/MenuController.cs
+++ Assassin´s Arena/Assets/MenuController.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+
+    [Header("Volume Settings")]
+    [SerializeField] private TMP_TextElement volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private GameObject confirmationPrompt = null;
+
+
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,4 +23,28 @@ public class MenuController : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
+
+    }
+
+
+
 }
