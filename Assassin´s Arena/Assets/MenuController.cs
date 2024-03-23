a655b7a5f6b0b155259139c5cf2891d3a102b327
Ryhrr <158263586+Ryhrr@users.noreply.github.com>
2024-03-23 18:01:29 +0100

    Map changes + Boarder
    Wasser sprite hinzugefügt und Border eingestellt map fertig endlich hot glob fucking 6 stunda brucht  XDXDXDD AHHAHAHAHAHHAA

--- Assassin´s Arena/Assets/MenuController.cs
+++ Assassin´s Arena/Assets/MenuController.cs
@@ -1,24 +1,38 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using TMPro;
 
 public class MenuController : MonoBehaviour
 {
-
     [Header("Volume Settings")]
-    [SerializeField] private TMP_TextElement volumeTextValue = null;
+    [SerializeField] private TMP_Text volumeTextValue = null;
     [SerializeField] private Slider volumeSlider = null;
     [SerializeField] private GameObject confirmationPrompt = null;
 
+    private void Start()
+    {
+        if (volumeTextValue == null || volumeSlider == null || confirmationPrompt == null)
+        {
+            Debug.LogError("One or more required components are not assigned.");
+            return;
+        }
 
+        if (!PlayerPrefs.HasKey("masterVolume"))
+        {
+            PlayerPrefs.SetFloat("masterVolume", 1.0f); // Default volume
+        }
+
+        volumeSlider.value = PlayerPrefs.GetFloat("masterVolume");
+        volumeTextValue.text = volumeSlider.value.ToString("0.0");
+    }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
     public void ExitButton()
     {
         Application.Quit();
@@ -28,7 +42,6 @@ public class MenuController : MonoBehaviour
     {
         AudioListener.volume = volume;
         volumeTextValue.text = volume.ToString("0.0");
-
     }
 
     public void VolumeApply()
@@ -37,14 +50,10 @@ public class MenuController : MonoBehaviour
         StartCoroutine(ConfirmationBox());
     }
 
-    public IEnumerator ConfirmationBox()
+    private IEnumerator ConfirmationBox()
     {
         confirmationPrompt.SetActive(true);
         yield return new WaitForSeconds(2);
         confirmationPrompt.SetActive(false);
-
     }
-
-
-
 }
