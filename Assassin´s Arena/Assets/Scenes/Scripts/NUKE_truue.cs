e394873bc362f13d260bb405df73d88ea8e679cc
Lucas0622 <158263752+Lucas0622@users.noreply.github.com>
2024-05-28 21:47:50 +0200

    Tobias bugges fixen hehe

--- Assassin´s Arena/Assets/Scenes/Scripts/NUKE_truue.cs
+++ Assassin´s Arena/Assets/Scenes/Scripts/NUKE_truue.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NUKE_truue : MonoBehaviour
 {
    
     public static bool nuuke = false;
-
+    
     void Start()
     {
         StartCoroutine(WaitAndDisableNuuke());
+        
     }
 
     IEnumerator WaitAndDisableNuuke()
@@ -17,6 +19,7 @@ public class NUKE_truue : MonoBehaviour
         nuuke = true;
         yield return new WaitForSeconds(2);
         nuuke = false;
+
     }
 }
 
