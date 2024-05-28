9a78fcba51518c964040f232fcd763ffa8207ad8
Lucas0622 <158263752+Lucas0622@users.noreply.github.com>
2024-05-28 14:29:10 +0200

    Reapply "asdf"
    This reverts commit 9e7e56cf60d696f2398781ad6ea16403c6709eb2.

---
+++ Assassin´s Arena/Assets/Scenes/Scripts/NUKE_truue.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NUKE_truue : MonoBehaviour
+{
+   
+    public static bool nuuke = false;
+
+    void Start()
+    {
+        StartCoroutine(WaitAndDisableNuuke());
+    }
+
+    IEnumerator WaitAndDisableNuuke()
+    {
+        nuuke = true;
+        yield return new WaitForSeconds(2);
+        nuuke = false;
+    }
+}
+
+
